b1fd1ae83b0d0cecaf9edcd28b10c9049c21ca83
Vincent Lin <vincentlin1617@gmail.com>
2026-01-15 16:39:58 -0500

    reptition question

--- cpp/cses/reptitions.cpp
+++ cpp/cses/reptitions.cpp
@@ -9,15 +9,29 @@ static inline void fast_io() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
+static void reptition(string s){
+  int l = 0, maxSize = 0, n = (int)s.size();
+
+  for(int r = 0; r < n; ++r){
+    if (s[l] != s[r]){
+      l = r;
+    } else {
+      maxSize = max(maxSize,r-l+1);
+    }
+  }
+  cout << maxSize << "\n";
+}
 
 static void solve_one() {
-
+  string s;
+  cin >> s;
+  reptition(s);
 }
 
 int main() {
     fast_io();
 
-    // solve_one();
+    solve_one();
     
 
     // int T; cin >> T;
